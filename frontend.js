498f2fdb33e221681616d7ab69c9dced5fa9ded7
sodablue1996 <sodablue1996@gmail.com>
2016-08-03 17:58:25 +0200

    Add files via upload

---
+++ frontend.js
@@ -0,0 +1,117 @@
+$(function () {
+    "use strict";
+
+    // for better performance - to avoid searching in DOM
+    var content = $('#content');
+    var input = $('#input');
+    var status = $('#status');
+    var ProductionDefinitions;
+/*var jsonfile = require('jsonfile')
+
+var file = '/chat/ProductionDefinitions.json'
+var ProductionDefinitions = jsonfile.readFileSync(file);*/
+
+
+    // if user is running mozilla then use it's built-in WebSocket
+    window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+    // if browser doesn't support WebSocket, just show some notification and exit
+    if (!window.WebSocket) {
+        content.html($('<p>', { text: 'Sorry, but your browser doesn\'t '
+                                    + 'support WebSockets.'} ));
+        input.hide();
+        $('span').hide();
+        return;
+    }
+
+    // open connection
+    var connection = new WebSocket('ws://127.0.0.1:1337');
+
+    connection.onopen = function () {
+        // first we want users to enter their names
+        input.removeAttr('disabled');
+        status.text('Request:');
+    };
+
+    connection.onerror = function (error) {
+        // just in there were some problems with conenction...
+        content.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
+                                    + 'connection or the server is down.' } ));
+    };
+
+    // most important part - incoming messages
+    connection.onmessage = function (message) {
+        // try to parse JSON message. Because we know that the server always returns
+        // JSON this should work without any problem but we should make sure that
+        // the massage is not chunked or otherwise damaged.
+        try {
+            var json = JSON.parse(message.data);
+       
+     
+        } catch (e) {
+            console.log('This doesn\'t look like a valid JSON: ', message.data);
+            return;
+        }
+
+        // NOTE: if you're not sure about the JSON structure
+        // check the server source code above
+
+        if (json.type === 'message') { // it's a single message
+            input.removeAttr('disabled'); // let the user write another message
+
+            addMessage(json.data);
+            if (json.data.includes('level')){
+            var level=json.data.split('level')[1];
+
+            addMessage(ProductionDefinitions.levels[level].exp);
+            }}
+       else if (json.type === 'ProductionDefinitions'){
+        
+       
+            ProductionDefinitions = json.data;
+            
+       }
+       else {
+            console.log('Hmm..., I\'ve never seen JSON like this: ', json);
+        }
+    };
+
+    /**
+     * Send mesage when user presses Enter key
+     */
+    input.keydown(function(e) {
+        if (e.keyCode === 13) {
+            var msg = $(this).val();
+            if (!msg) {
+                return;
+            }
+            // send the message as an ordinary text
+            connection.send(msg);
+            $(this).val('');
+            // disable the input field to make the user wait until server
+            // sends back response
+            input.attr('disabled', 'disabled');
+
+        }
+    });
+
+    /**
+     * This method is optional. If the server wasn't able to respond to the
+     * in 3 seconds then show some error message to notify the user that
+     * something is wrong.
+     */
+    setInterval(function() {
+        if (connection.readyState !== 1) {
+            status.text('Error');
+            input.attr('disabled', 'disabled').val('Unable to comminucate '
+                                                 + 'with the WebSocket server.');
+        }
+    }, 3000);
+
+    /**
+     * Add message to the chat window
+     */
+    function addMessage(message) {
+              document.getElementById("content").innerHTML = document.getElementById("content").innerHTML+'<br/>'+ message;
+    }
+});
